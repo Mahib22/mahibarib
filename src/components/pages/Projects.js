d94b00af79f09d536a0916eeb275d4d86f9fbcae
Mahib22 <mahib.arib@gmail.com>
2022-08-17 22:26:19 +0700

    typo

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+// eslint-disable-next-line no-unused-vars
 import microtip from "microtip/microtip.css";
 import TitleSection from "../section/TitleSection";
 import PortfolioItem from "../section/PortfolioItem";
